adbc590e362acb259177a48a6ef64428a15ffefb
WaterMuffin <narlor200@gmail.com>
2024-09-27 22:08:10 +0300

    asd

--- js/index.js
+++ js/index.js
@@ -6,45 +6,110 @@ const COLORS = {
 	correct: '#1adf00'
 }
 
+let answers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+let answers_count = 0
+
 const QUESTIONS = [
 	{
 		id: 'moscow',
-		description: 'moscow',
-		checked: false
+		description: 'Где находится Москва?'
 	},
 
 	{
 		id: 'spb',
-		description: 'spb',
-		checked: false
+		description: 'Где находится Санкт-Петербург?'
 	},
 	
 	{
 		id: 'voronez',
-		description: 'voronez',
-		checked: false
+		description: 'Где находится Воронеж?'
+	},
+
+	{
+		id: 'nizni-novgorod',
+		description: 'Где находится Нижний Новгород?'
+	},
+	
+	{
+		id: 'rostovND',
+		description: 'Где находится Ростов-на-Дону?'
+	},
+	
+	{
+		id: 'volgograd',
+		description: 'Где находится Волгоград?'
+	},
+	
+	{
+		id: 'kazan',
+		description: 'Где находится Казань?'
+	},
+	
+	{
+		id: 'samara',
+		description: 'Где находится Самара?'
+	},
+	
+	{
+		id: 'ufa',
+		description: 'Где находится Уфа?'
+	},
+	
+	{
+		id: 'perm',
+		description: 'Где находится Пермь?'
+	},
+	
+	{
+		id: 'ekaterinburg',
+		description: 'Где находится Екатеринбург?'
+	},
+	
+	{
+		id: 'chelabinsk',
+		description: 'Где находится Челябинск?'
+	},
+	
+	{
+		id: 'omsk',
+		description: 'Где находится Омск?'
+	},
+	
+	{
+		id: 'novosibirsk',
+		description: 'Где находится Новосибирск?'
+	},
+
+	{
+		id: 'krasnoiarsk',
+		description: 'Где находится Красноярск?'
 	}
 ]
 
-function getRandomQuestion(max) {
-	let rnd = Math.floor(Math.random() * max)
-	return QUESTIONS[rnd]
+function getRandomQuestion() {
+	let rnd = Math.floor(Math.random() * answers.length)
+	let rnd_item = answers[rnd]
+	let rand = rnd_item
+	answers.splice(rnd, 1)
+	return QUESTIONS[rand]
 }
 
-let rndQuest = getRandomQuestion(3)
+let rndQuest = getRandomQuestion()
+let questDescript = document.getElementById('questDescription')
+questDescript.textContent = rndQuest.description
 
 function checkAnswer(cityName) {
-	let questDescript = document.getElementById('questDescription')
+	questDescript = document.getElementById('questDescription')
 	let city = document.getElementById(cityName)
 
 	if (rndQuest.id === cityName) {
 		city.style.fill = COLORS.correct
-		rndQuest.checked = true
-
-		do {
-			rndQuest = getRandomQuestion(3)
-		} while (rndQuest.checked === true); //или все объекты массива QUESTIONS имеют checked: true
-		
+		++answers_count
+		if (answers_count === 15) {
+			questDescript.textContent = 'Молодец! Ты победил!'
+		}
+		rndQuest = getRandomQuestion()
 		questDescript.textContent = rndQuest.description
 	}
 }
